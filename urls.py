c7c2152abf2307f47ba75dbaebcadc46863c7b9a
Akshar Raaj <akshar@agiliq.com>
2011-07-29 15:09:48 +0530

    Project starts

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'bank_center.views.home', name='home'),
+    # url(r'^bank_center/', include('bank_center.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
