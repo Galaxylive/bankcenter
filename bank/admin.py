856fbde47570a6766eac74076ba3df4d4f741a38
Akshar Raaj <akshar@agiliq.com>
2011-07-29 16:43:09 +0530

    models.py and admin.py for bank app created

---
+++ bank/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django import forms
+from models import Branch,Bank,Location
+
+"""ifsc code of Branch should be of fixed length i.e of 11 characters.
+    This can not be done with models.CharField, so we create
+    a ModelForm named BranchForm which lets us choose max_length
+    as well as min_length"""
+    
+class BranchForm(forms.ModelForm):
+    ifsc=forms.CharField(max_length=11,min_length=11) #This makes ifsc of fixed length of 11 characters.
+    
+    class Meta:
+        model=Branch
+        
+class BranchAdmin(admin.ModelAdmin):
+    form=BranchForm
+    
+admin.site.register(Branch,BranchAdmin)
+admin.site.register(Bank)
+admin.site.register(Location)
+    
--- bank/models.py
+++ bank/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    city=models.CharField(max_length=200)
+    district=models.CharField(max_length=200)
+    state=models.CharField(max_length=200)
+    
+    def __unicode__(self):
+        return "{0}, {1}".format(self.city,self.state)
+        
+class Bank(models.Model):
+    bank_name=models.CharField(max_length=300)
+    
+    def __unicode__(self):
+        return self.bank_name
+    
+class Branch(models.Model):
+    branch_name=models.CharField(max_length=300)
+    ifsc=models.CharField(max_length=11)
+    micr=models.CharField(max_length=9,blank=True,null=True) #We will set invalid micr codes to None which will be stored as null in database
+    address=models.CharField(max_length=300)
+    contact=models.CharField(max_length=300)
+    bank=models.ForeignKey(Bank)
+    location=models.ForeignKey(Location)
+    
+    def __unicode__(self):
+        return "{0}, {1}, {2}".format(self.bank,self.branch_name,self.location)
+        
+    class Meta:
+        verbose_name_plural='Branches'
+        
+
+    
