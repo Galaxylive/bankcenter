251397e50eb22d99a8d8827319f368f54fff2aa2
Akshar Raaj <akshar@agiliq.com>
2011-08-01 23:49:15 +0530

    details page for branch functioning properly, bank, state and city names link to respective filtering pages

--- bank/urls.py
+++ bank/urls.py
@@ -3,9 +3,11 @@ from bank import views
 
 urlpatterns = patterns('',
     url(r'^$',views.home,name='bank_home'),
-    url(r'^(?P<bank_slug>[a-zA-Z0-9_-]+)/$',views.bank_branches,name='bank_branches'),
-    #url(r'^(?P<city_name>[a-zA-Z0-9_-]+)/$',views.city_branches,name='city branches'),
+    url(r'^(?P<bank_slug>[a-zA-Z0-9_-]+)/$',views.bank_branches,name='bank_bank_branches'),
+    url(r'^banks/city/(?P<location_slug>[a-zA-Z0-9_-]+)/$',views.city_branches,name='bank_city_branches'),
     url(r'^(?P<bank_slug>[a-zA-Z0-9_-]+)/(?P<branch_slug>[a-zA-Z0-9_-]+)/$',views.branch_info,name='bank_branch_info'),
+    url(r'^banks/state/(?P<state_slug>[a-zA-Z0-9_-]+)/$',views.state_branches,name='bank_state_branches'),
+    
 )
 
 
--- bank/views.py
+++ bank/views.py
@@ -1,22 +1,34 @@
 # Create your views here.
-from models import Bank,Branch,Location
+from models import Bank,Branch,Location,State
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.shortcuts import render
 from django.template import RequestContext
 
 def home(request):
-    bank_list=Bank.objects.all()
+    bank_list=Bank.objects.select_related().all()
     return render(request,"bank/home.html")
     
 def bank_branches(request,bank_slug):
     #return HttpResponse("You are at {0}".format(bank_slug))
-    given_bank=Bank.objects.get(slug=bank_slug)
-    branch_list=Branch.objects.filter(bank=given_bank)
+    given_bank=Bank.objects.select_related().get(slug=bank_slug)
+    branch_list=Branch.objects.select_related().filter(bank=given_bank)
     #return HttpResponse("You are at {0}".format(bank_slug))
     return render(request,"bank/bank_branches.html",{'bank':given_bank,'branch_list':branch_list})
     
 def branch_info(request,bank_slug,branch_slug):
     #return HttpResponse("")
-    branch=Branch.objects.get(slug=branch_slug,bank__slug=bank_slug)
+    branch=Branch.objects.select_related().get(slug=branch_slug,bank__slug=bank_slug)
     return render_to_response("bank/branch_info.html",{'branch':branch},context_instance=RequestContext(request))
+    
+def city_branches(request,location_slug):
+    #return HttpResponse("You are at {0}".format(city_name))
+    branch_list=Branch.objects.select_related().filter(location__slug=location_slug)
+    #return HttpResponse("You are at {0}".format(city_name))
+    return render(request,'bank/city_branches.html',{'location_slug':location_slug,'branch_list':branch_list})
+    
+def state_branches(request,state_slug):
+    #return HttpResponse('You are at {0}'.format(state_slug)
+    branch_list=Branch.objects.select_related().filter(location__state_fk__slug=state_slug)
+    #return HttpResponse('You are at {0}'.format(state_slug))
+    return render(request,'bank/state_branches.html',{'location_slug':state_slug,'branch_list':branch_list})
