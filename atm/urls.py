d8ffe0bf31cc9c272299392cab1d73cc15267458
Saket <pure.vintage.skt@gmail.com>
2011-08-24 18:56:13 +0530

    #30 linked atms and pincodes

--- atm/urls.py
+++ atm/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 from atm import views
 
 urlpatterns = patterns('',
-    url(r'^atms$', views.atms, name='atms'),
+    url(r'^atms$', views.atms, name='atm_home'),
     url(r'^atms/city/(?P<city_id>[\w]+)/$', views.city, name='city'),                                         
     url(r'^atms/bank/(?P<bank_id>[\w]+)/$', views.city, name='bank'),                                         
     #url(r'^detail$', views.detail, name='detail'),
