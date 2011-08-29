b539a4a7295c2440d9f794c66a69386e673fc3fa
Shabda Raaj <shabda@agiliq.com>
2011-08-29 13:57:49 +0530

    Group imports

--- atm/views.py
+++ atm/views.py
@@ -1,11 +1,11 @@
-# Create your views here.
-from models import Atm
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.shortcuts import render
 from django.template import RequestContext
 from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 from django.http import Http404
+
+from models import Atm
 from utils import get_letters
 
 def render(request, template, context):
