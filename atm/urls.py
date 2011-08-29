729150676d6de4eaf23cd97e8e5b4b823f30af5c
Shabda Raaj <shabda@agiliq.com>
2011-08-29 13:43:17 +0530

    Better names for urlpatterns. Refs #43.

--- atm/urls.py
+++ atm/urls.py
@@ -3,9 +3,9 @@ from atm import views
 
 urlpatterns = patterns('',
     url(r'^$', views.atms, name='atm_home'),
-    url(r'^city/(?P<city_id>[\w-]+)/$', views.city, name='city'),                                         
-    url(r'^bank/(?P<bank_id>[\w-]+)/$', views.bank, name='bank'),                                         
-    url(r'^(?P<detail_id>[a-zA-Z0-9-/]+)/$', views.detail, name='detail'),                                                                
+    url(r'^city/(?P<city_id>[\w-]+)/$', views.city, name='atms_in_city'),                                         
+    url(r'^bank/(?P<bank_id>[\w-]+)/$', views.bank, name='atms_for_bank'),                                         
+    url(r'^(?P<detail_id>[a-zA-Z0-9-/]+)/$', views.detail, name='atm_detail'),                                                                
     #url(r'^detail$', views.detail, name='detail'),
     #url(r'^pincode/city/(?P<city_id>[\w]+)/$', views.city, name='city'),                      
 )
