ce145273c85ed8ed93dd7cbd4c47b427ed48083c
Shabda Raaj <shabda@agiliq.com>
2011-08-29 13:51:10 +0530

    Refs #44. Use reverse.

--- atm/urls.py
+++ atm/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     url(r'^$', views.atms, name='atm_home'),
     url(r'^city/(?P<city_id>[\w-]+)/$', views.city, name='atms_in_city'),                                         
     url(r'^bank/(?P<bank_id>[\w-]+)/$', views.bank, name='atms_for_bank'),                                         
-    url(r'^(?P<detail_id>[a-zA-Z0-9-/]+)/$', views.detail, name='atm_detail'),                                                                
+    url(r'^(?P<city_id>[\w]+)/(?P<bank_id>[\w-]+)/(?P<detail_id>[0-9-/]+)/$', views.detail, name='atm_detail'),                                                                
     #url(r'^detail$', views.detail, name='detail'),
     #url(r'^pincode/city/(?P<city_id>[\w]+)/$', views.city, name='city'),                      
 )
