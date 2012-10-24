aec09e04195876d13445d6350f6ad88ccbad6d9d
Akshar Raaj <akshar@agiliq.com>
2012-10-25 01:14:27 +0530

    Remove unnecessary blank lines

--- bank/context_processors.py
+++ bank/context_processors.py
@@ -5,8 +5,3 @@ def required_context(request):
     location_list=Location.objects.all()
     return {'bank_list':bank_list,'location_list':location_list}
     #return {'bank_list':bank_list}
-    
-
-    
-  
-  
