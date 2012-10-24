ed78a06f58cf77fb5f7389aac5152a99b243c5ca
Akshar Raaj <akshar@agiliq.com>
2012-10-25 01:30:19 +0530

    bank/context_processors confirms to pep8

--- bank/context_processors.py
+++ bank/context_processors.py
@@ -1,7 +1,7 @@
-from models import Bank,Location
+from models import Bank, Location
+
 
 def required_context(request):
-    bank_list=Bank.objects.all()
-    location_list=Location.objects.all()
-    return {'bank_list':bank_list,'location_list':location_list}
-    #return {'bank_list':bank_list}
+    bank_list = Bank.objects.all()
+    location_list = Location.objects.all()
+    return {'bank_list': bank_list, 'location_list': location_list}
