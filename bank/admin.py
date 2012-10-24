879e2df92f88c8e98d3e638bed5dac218048ad5b
Akshar Raaj <akshar@agiliq.com>
2012-10-25 01:27:48 +0530

    bank/admin confirms to pep8

--- bank/admin.py
+++ bank/admin.py
@@ -1,25 +1,29 @@
 from django.contrib import admin
 from django import forms
-from models import Branch,Bank,Location,State
+
+from models import Branch, Bank, Location, State
 
 """ifsc code of Branch should be of fixed length i.e of 11 characters.
     This can not be done with models.CharField, so we create
     a ModelForm named BranchForm which lets us choose max_length
     as well as min_length"""
-    
+
+
 class BranchForm(forms.ModelForm):
-    ifsc=forms.CharField(max_length=11,min_length=11) #This makes ifsc of fixed length of 11 characters.
-    
+    #This makes ifsc of fixed length of 11 characters.
+    ifsc = forms.CharField(max_length=11, min_length=11)
+
     class Meta:
-        model=Branch
-        
+        model = Branch
+
+
 class BranchAdmin(admin.ModelAdmin):
-    prepopulated_fields={'slug':('branch_name',)}
-    form=BranchForm
-    search_fields=['bank__bank_name']
-    
-admin.site.register(Branch,BranchAdmin)
+    prepopulated_fields = {'slug': ('branch_name',)}
+    form = BranchForm
+    search_fields = ['bank__bank_name']
+
+
+admin.site.register(Branch, BranchAdmin)
 admin.site.register(Bank)
 admin.site.register(Location)
 admin.site.register(State)
-    
