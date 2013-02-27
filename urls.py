5ed1a9a9677ce8d78fd6d05561cca20521d833af
Akshar Raaj <akshar@agiliq.com>
2013-02-28 00:07:27 +0530

    Add url for sitemap

--- urls.py
+++ urls.py
@@ -1,15 +1,30 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.sitemaps import GenericSitemap
 
 from django.contrib import admin
 admin.autodiscover()
+
+from bank.models import Branch
+branch_dict = {
+    'queryset': Branch.objects.all()[:10],
+    'date_field': 'last_accessed' 
+}
+
+sitemaps = {
+    'branches': GenericSitemap(branch_dict, priority=0.5)
+}
 
 urlpatterns = patterns('',
      url(r'^admin/', include(admin.site.urls)),
      url(r'^atms/', include('atm.urls')),
      (r'^', include('zipcode.urls')),
      (r'^search/', include('haystack.urls')),
-     (r'^', include('bank.urls')),
+     (r'^bank/', include('bank.urls')),
+)
+
+urlpatterns += patterns('',
+    (r'^sitemap\.xml/$', 'django.contrib.sitemaps.views.sitemap',  {'sitemaps': sitemaps}),
 )
 
 urlpatterns += staticfiles_urlpatterns()
