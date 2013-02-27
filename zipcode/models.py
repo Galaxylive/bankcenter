386d01b225c701bcfd5ce64de831f80386d4023f
Akshar Raaj <akshar@agiliq.com>
2013-02-28 01:20:02 +0530

    pep8 changes

--- zipcode/models.py
+++ zipcode/models.py
@@ -3,15 +3,15 @@ from django.db import models
 
 
 class Zip_code(models.Model):
-    post_office_name=models.CharField(max_length=50)
-    pin_code=models.CharField(max_length=10)
-    district_name=models.CharField(max_length=50)
-    city_name=models.CharField(max_length=50)
-    state=models.CharField(max_length=50)
+    post_office_name = models.CharField(max_length=50)
+    pin_code = models.CharField(max_length=10)
+    district_name = models.CharField(max_length=50)
+    city_name = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
     latitude = models.FloatField()
-    longitude = models.FloatField() 
+    longitude = models.FloatField()
     city_slug = models.SlugField(max_length=100)
-    
+
     def __unicode__(self):
         return self.post_office_name
 
