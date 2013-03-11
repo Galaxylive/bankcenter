64839bbfe4c2df0048dc2a530ed159b8be969a42
Akshar Raaj <akshar@agiliq.com>
2013-03-11 23:59:00 +0530

    pep8 changes

--- bank/search_indexes.py
+++ bank/search_indexes.py
@@ -1,6 +1,7 @@
 from haystack.indexes import *
 from haystack import site
-from bank.models import Location, Bank, Branch
+
+from bank.models import Branch
 
 class BranchIndex(SearchIndex):
     branch_name = CharField(document=True, use_template=True)
