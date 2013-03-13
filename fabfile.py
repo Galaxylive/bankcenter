2aa2b1ff2b7f08894390c13e18d04a748de2cbd9
Akshar Raaj <akshar@agiliq.com>
2013-03-13 17:48:24 +0530

    Updated fabfile

--- fabfile.py
+++ fabfile.py
@@ -3,36 +3,44 @@ from __future__ import with_statement
 from fabric.api import *
 from fabric.operations import run, sudo
 from fabric.context_managers import cd, settings
-from fabric.api import env
+from fabric.api import env, prefix
 from fabric.colors import green
-HOST_NAME = ['www.bankcenter.in']
-USER = "akshar"
+HOST_NAME = ['bankcenter.in']
+USER = "bankcenter_in"
 REPO_PATH = "~/bankcenter/bank_center"
 
 env.hosts = HOST_NAME
 env.user = USER
 env.directory = REPO_PATH
+env.activate = 'source ~/bankcenter/bin/activate'
 
 def restart():
     print(green("Restarting...\n"))
-    run("sudo service apache2 restart")
+    run("gunicorn_django -c ~/confs/gunicorn/gunicorn_cfg.py")
     print(green("Restarted\n"))
+
+def kill_process():
+    print(green("Killing gunicorn process...\n"))
+    run("kill `cat ~/confs/gunicorn/pid.txt`")
+    print("Killed gunicorn process")
+
 def syncdb():
     print(green("syncdb is running...\n"))
     run("python manage.py syncdb")
+
 def migrate():
     print(green("Migrating database...\n"))
     run("python manage.py migrate")
+
 def pull():
     print(green("Pulling the latest code...\n"))
     run("git pull")
-def deploy():
-    with settings(user=USER):
-        with cd(env.directory):
-           pull()
-           migrate()
-           syncdb()
-           restart()
-if __name__ == "__main__":
-   deploy()
 
+def deploy():
+    with cd(env.directory):
+        with prefix(env.activate):
+            pull()
+            migrate()
+            syncdb()
+            kill_process()
+            restart()
