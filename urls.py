72a05140b08c22e7db0a77bf266e22ef05423617
Ashik Sujath <ashik@agiliq.com>
2014-06-06 20:01:24 +0530

    robots.txt to be served through cbv

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.sitemaps import GenericSitemap
-# from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -16,18 +16,18 @@ sitemaps = {
     'branches': GenericSitemap(branch_dict, priority=0.5)
 }
 
+
 urlpatterns = patterns('',
-     url(r'^$', HomeView.as_view(), name='home'),
-     # url(r'^robots\.txt$', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'}),
-     url(r'^admin/', include(admin.site.urls)),
-     url(r'^atms/', include('atm.urls')),
-     (r'^pincode/', include('zipcode.urls')),
-     (r'^search/', include('haystack.urls')),
-     (r'^bank/', include('bank.urls')),
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    # url(r'^robots\.txt$', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'}),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^atms/', include('atm.urls')),
+    (r'^pincode/', include('zipcode.urls')),
+    (r'^search/', include('haystack.urls')),
+    (r'^bank/', include('bank.urls')),
 )
 
 urlpatterns += patterns('',
     (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',  {'sitemaps': sitemaps}),
 )
-
-# To Do: add url for robots.txt
