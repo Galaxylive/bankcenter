54b8e8d743404e81784b81b31bd25b71658d1889
Ashik Sujath <ashik@agiliq.com>
2014-06-06 16:11:25 +0530

    more fbv converted to cbv in bank app

--- bank/urls.py
+++ bank/urls.py
@@ -10,11 +10,10 @@ urlpatterns = patterns('',
     url(r'^ifsc/(?P<branch_ifsc>[A-Z0-9]+)/$', views.BranchIfscView.as_view(), name='bank_branch_with_ifsc'),
     url(r'^micr/(?P<branch_micr>[0-9]+)/$', views.BranchMicrView.as_view(), name='bank_branch_with_micr'),
     url(r'^city/(?P<location_slug>[a-zA-Z0-9_-]+)/$', views.CityBranchesView.as_view(), name='bank_city_branches'),
-
-    url(r'^state/(?P<state_slug>[a-zA-Z0-9_-]+)/$', views.state_branches, name = 'bank_state_branches'),
-    url(r'^state/(?P<state_slug>[a-zA-Z0-9_-]+)/(?P<bank_slug>[a-zA-Z0-9_-]+)/$', views.bank_state_branches, name = 'bank_bank_state_branches'),
-    url(r'^(?P<bank_slug>[a-zA-Z0-9_-]+)/$', views.bank_branches, name = 'bank_bank_branches'),
-    url(r'^(?P<bank_slug>[a-zA-Z0-9_-]+)/(?P<location_slug>[a-zA-Z0-9_-]+)/$', views.bank_city_branches, name = 'bank_bank_city_branches'),
+    url(r'^state/(?P<state_slug>[a-zA-Z0-9_-]+)/$', views.StateBranchesView.as_view(), name='bank_state_branches'),
+    url(r'^state/(?P<state_slug>[a-zA-Z0-9_-]+)/(?P<bank_slug>[a-zA-Z0-9_-]+)/$', views.BankStateBranchesView.as_view(), name='bank_bank_state_branches'),
+    url(r'^(?P<bank_slug>[a-zA-Z0-9_-]+)/$', views.BankBranchesView.as_view(), name='bank_bank_branches'),
+    url(r'^(?P<bank_slug>[a-zA-Z0-9_-]+)/(?P<location_slug>[a-zA-Z0-9_-]+)/$', views.BankCityBranchesView.as_view(), name='bank_bank_city_branches'),
     # The regex below catches everything, come here in case of doubt.
-    url(r'^(?P<bank_slug>[a-zA-Z0-9_-]+)/(?P<branch_slug>[a-zA-Z0-9_-]+)/(?P<branch_ifsc>[A-Z0-9]+)/$', views.branch_info, name = 'bank_branch_info'),
+    url(r'^(?P<bank_slug>[a-zA-Z0-9_-]+)/(?P<branch_slug>[a-zA-Z0-9_-]+)/(?P<branch_ifsc>[A-Z0-9]+)/$', views.BranchInfoView.as_view(), name='bank_branch_info'),
 )
--- bank/views.py
+++ bank/views.py
@@ -1,15 +1,10 @@
-from django.shortcuts import render_to_response
-from django.shortcuts import render
-from django.template import RequestContext
 from django.http import Http404
 from django.db.models import Count
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import TemplateView, ListView, DetailView
 
 from .models import Bank, Branch, Location, State
 from .utils import get_letters
-
-
-from django.views.generic import TemplateView, ListView, DetailView
 
 
 class HomeView(TemplateView):
@@ -45,7 +40,7 @@ class CitiesView(ListView):
     def get_queryset(self):
         letter = self.request.GET.get('letter', 'A')
         location_list = Location.objects.select_related().filter(
-                        city__startswith=letter)
+            city__startswith=letter)
         return location_list
 
 
@@ -104,120 +99,114 @@ class CityBranchesView(ListView):
         return context
 
 
-# def home(request):
-#     branches = Branch.objects.all()[:10]
-#     banks = Bank.objects.select_related().all().order_by(
-#         '-num_branches')[:21]
-#     locations = Location.objects.select_related().all().\
-#         order_by('-num_branches')[:21]
-#     return render(
-#         request, "bank/home.html",
-#         {'branch_list': branches, 'bank_list': banks,
-#             'location_list': locations})
+class StateBranchesView(TemplateView):
+    template_name = 'bank/state_branches.html'
 
-def bank_branches(request, bank_slug):
-    try:
-        given_bank = Bank.objects.select_related().get(slug=bank_slug)
-    except Bank.DoesNotExist:
-        raise Http404
-    branch_list = Branch.objects.select_related().filter(bank=given_bank)
+    def get_context_data(self, **kwargs):
+        state_slug = self.kwargs.get('state_slug', '')
+        state = State.objects.get(slug=state_slug)
+        banks = Branch.objects.filter(location__state_fk=state).values('bank__bank_name', 'bank__slug').annotate(dcount=Count('bank__bank_name'))
+        context = super(StateBranchesView, self).get_context_data(**kwargs)
+        context['banks'] = banks
+        context['state'] = state
+        return context
 
-    paginator = Paginator(branch_list, 20)
-    page = int(request.GET.get('page', 1))
 
-    try:
-        branch_list = paginator.page(page)
-    except PageNotAnInteger:
-        branch_list = paginator.page(1)
-    except EmptyPage:
-        branch_list = paginator.page(paginator.num_pages)
-    max_range = page+3
-    if max_range > paginator.num_pages:
-        max_range = paginator.num_pages+1
-    pages = range(page-3, max_range)
-    pages = [pagenum for pagenum in pages if pagenum>0]
-    return render(request, "bank/bank_branches.html", {'bank':given_bank, 'branch_list':branch_list, 'pages': pages})
+class BankStateBranchesView(TemplateView):
+    template_name = 'bank/bank_state_branches.html'
 
-def branch_info(request, bank_slug, branch_slug, branch_ifsc):
-    try:
-        branch=Branch.objects.select_related().get(slug=branch_slug, bank__slug=bank_slug, ifsc=branch_ifsc)
-        bank=Bank.objects.get(slug=bank_slug)
-        bank.num_times_accessed += 1
-        bank.save()
-        loc=branch.location
-        loc.num_times_accessed += 1
-        loc.save()
-        branch.save() #Each time this branch gets accessed, we call save() so that last_accessed field gets updated for this branch.
-    except Branch.DoesNotExist, Bank.DoesNotExist:
-        raise Http404
-    return render_to_response("bank/branch_info.html",{'branch':branch},context_instance=RequestContext(request))
+    def get_context_data(self, **kwargs):
+        state_slug = self.kwargs.get('state_slug', '')
+        bank_slug = self.kwargs.get('bank_slug', '')
+        context = super(BankStateBranchesView, self).get_context_data(**kwargs)
+        try:
+            branch_list = Branch.objects.select_related().filter(bank__slug=bank_slug, location__state_fk__slug=state_slug)
+            bank = Bank.objects.get(slug=bank_slug)
+            bank.num_times_accessed += 1
+            bank.save()
+        except Bank.DoesNotExist:
+            raise Http404
+        context.update({'branch_list': branch_list, 'bank': bank})
+        return context
 
-# def city_branches(request, location_slug):
-#     branch_list = Branch.objects.select_related().filter(location__slug=location_slug)
-#     return render(request, 'bank/city_branches.html', {'location_slug':location_slug, 'branch_list':branch_list})
 
-def state_branches(request, state_slug):
-    state = State.objects.get(slug=state_slug)
-    banks = Branch.objects.filter(location__state_fk=state).values('bank__bank_name', 'bank__slug').annotate(dcount=Count('bank__bank_name'))
-    return render(request, 'bank/state_branches.html', {'banks':banks, 'state':state})
+class BankBranchesView(TemplateView):
+    template_name = "bank/bank_branches.html"
 
-# def cities(request):
-#     letter = request.GET.get('letter', 'A')
-#     location_list = Location.objects.select_related().filter(
-#         city__startswith=letter)
-#     letters = get_letters()
-#     return render(
-#         request, 'bank/cities.html',
-#         {'location_list': location_list, 'letters': letters})
+    def get(self, request, *args, **kwargs):
+        self.context_data = {}
+        bank_slug = kwargs.get('bank_slug', '')
+        try:
+            given_bank = Bank.objects.select_related().get(slug=bank_slug)
+        except Bank.DoesNotExist:
+            raise Http404
 
-# def banks(request):
-#     banks = Bank.objects.select_related().all()
-#     return render(request, "bank/banks.html", {'bank_list': banks})
+        branch_list = Branch.objects.select_related().filter(bank=given_bank)
 
-# def branch_with_ifsc(request, branch_ifsc):
-#     try:
-#         branch = Branch.objects.select_related().get(ifsc=branch_ifsc)
-#         bank = branch.bank
-#         bank.num_times_accessed += 1
-#         bank.save()
-#         loc = branch.location
-#         loc.num_times_accessed += 1
-#         loc.save()
-#         branch.save()
-#     except Branch.DoesNotExist:
-#         raise Http404
-#     return render(request, 'bank/branch_info.html', {'branch': branch})
+        paginator = Paginator(branch_list, 20)
+        page = int(request.GET.get('page', 1))
 
-# def branch_with_micr(request, branch_micr):
-#     try:
-#         branch = Branch.objects.select_related().get(micr=branch_micr)
-#         bank = branch.bank
-#         bank.num_times_accessed += 1
-#         bank.save()
-#         loc = branch.location
-#         loc.num_times_accessed += 1
-#         loc.save()
-#         branch.save()
-#     except Branch.DoesNotExist:
-#         raise Http404
-#     return render(request, 'bank/branch_info.html', {'branch': branch})
+        try:
+            branch_list = paginator.page(page)
+        except PageNotAnInteger:
+            branch_list = paginator.page(1)
+        except EmptyPage:
+            branch_list = paginator.page(paginator.num_pages)
 
-def bank_city_branches(request, bank_slug, location_slug):
-    try:
-        branch_list = Branch.objects.select_related().filter(bank__slug=bank_slug, location__slug=location_slug)
-        bank = Bank.objects.get(slug=bank_slug)
-        bank.num_times_accessed += 1
-        bank.save()
-    except Bank.DoesNotExist:
-        raise Http404
-    return render(request, 'bank/bank_city_branches.html', {'branch_list':branch_list, 'bank': bank})
+        max_range = page+3
+        if max_range > paginator.num_pages:
+            max_range = paginator.num_pages+1
+        pages = range(page-3, max_range)
+        pages = [pagenum for pagenum in pages if pagenum > 0]
+        self.context_data['branch_list'] = branch_list
+        self.context_data['bank'] = given_bank
+        self.context_data['pages'] = pages
+        return super(BankBranchesView, self).get(request, *args, **kwargs)
 
-def bank_state_branches(request, state_slug, bank_slug):
-    try:
-        branch_list = Branch.objects.select_related().filter(bank__slug=bank_slug, location__state_fk__slug=state_slug)
-        bank = Bank.objects.get(slug=bank_slug)
-        bank.num_times_accessed += 1
-        bank.save()
-    except Bank.DoesNotExist:
-        raise Http404
-    return render(request, 'bank/bank_state_branches.html', {'branch_list':branch_list, 'bank': bank})
+    def get_context_data(self, **kwargs):
+        context = super(BankBranchesView, self).get_context_data(**kwargs)
+        context.update(self.context_data)
+        return context
+
+
+class BankCityBranchesView(ListView):
+    template_name = 'bank/bank_city_branches.html'
+
+    def get_queryset(self):
+        bank_slug = self.kwargs.get('bank_slug', '')
+        location_slug = self.kwargs.get('location_slug', '')
+        try:
+            branch_list = Branch.objects.select_related().filter(bank__slug=bank_slug, location__slug=location_slug)
+            self.bank = Bank.objects.get(slug=bank_slug)
+            self.bank.num_times_accessed += 1
+            self.bank.save()
+        except Bank.DoesNotExist:
+            raise Http404
+        return branch_list
+
+    def get_context_data(self, **kwargs):
+        context = super(BankCityBranchesView, self).get_context_data(**kwargs)
+        context['bank'] = self.bank
+        return context
+
+
+class BranchInfoView(DetailView):
+    template_name = "bank/branch_info.html"
+    context_object_name = 'branch'
+
+    def get_object(self, queryset=None):
+        branch_slug = self.kwargs.get('branch_slug', '')
+        bank_slug = self.kwargs.get('bank_slug', '')
+        branch_ifsc = self.kwargs.get('branch_ifsc', '')
+        try:
+            branch = Branch.objects.select_related().get(slug=branch_slug, bank__slug=bank_slug, ifsc=branch_ifsc)
+            bank = Bank.objects.get(slug=bank_slug)
+            bank.num_times_accessed += 1
+            bank.save()
+            loc = branch.location
+            loc.num_times_accessed += 1
+            loc.save()
+            branch.save()  # Each time this branch gets accessed, we call save() so that last_accessed field gets updated for this branch.
+        except Branch.DoesNotExist, Bank.DoesNotExist:
+            raise Http404
+        return branch
